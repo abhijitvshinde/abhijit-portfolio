811c577828fd52e5626150dd0aef76b27b3250e1
Abhijit Shinde <79440599+abhijitvshinde@users.noreply.github.com>
2026-05-25 21:34:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function toggleMenu() {
+  document.getElementById("navLinks").classList.toggle("show");
+}
 
+function revealSections() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  reveals.forEach((section) => {
+    const windowHeight = window.innerHeight;
+    const revealTop = section.getBoundingClientRect().top;
+    const revealPoint = 100;
+
+    if (revealTop < windowHeight - revealPoint) {
+      section.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealSections);
+
+revealSections();
